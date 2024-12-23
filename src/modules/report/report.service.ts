253512e67de02b7ddff2358f5d80fd1065691f42
clecio <oclecioalvessousa@gmail.com>
2024-12-23 15:30:10 -0300

    chore: mudando tempo pra gerar relatório

--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -8,8 +8,8 @@ export class ReportService {
   constructor(@InjectQueue('report') private readonly reportQueue: Queue) {}
 
   //@Cron('59 23 * * *') // Executa às 23:59 todos os dias
-  @Cron('*/30 * * * * *') // Executa a cada 30 segundos
-  //@Cron('0 */3 * * *') // Executa a cada 3 horas
+  //@Cron('*/30 * * * * *') // Executa a cada 30 segundos
+  @Cron('0 */3 * * *') // Executa a cada 3 horas
   async scheduleDailyReport() {
     await this.reportQueue.add('generate-report', {}, { delay: 0 });
   }
